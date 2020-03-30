a8a2273128e4e5cf3532c2fdeb3ffde54b8e2ff7
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:35:38 +0200

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,43 @@
+﻿namespace StaticExamples
+{
+    /// <summary>
+    /// A very simple representation of a car
+    /// </summary>
+    public class Car
+    {
+        #region Instance fields
+        private string _licensePlate;
+        private int _price;
+        private static int _numberOfCars;
+        private static int _useOfLicence;
+        private static int _useOfPrice;
+        #endregion
+
+        #region Constructor
+        public Car(string licensePlate, int price)
+        {
+            _licensePlate = licensePlate;
+            _price = price;
+            _numberOfCars++;
+        }
+        #endregion
+
+        #region Properties
+        public string LicensePlate
+        {
+            get { _useOfLicence++; return _licensePlate; }
+            set { _licensePlate = value; _useOfLicence++;}
+        }
+
+        public int Price
+        {
+            get { _useOfPrice++; return _price; }
+            set { _price = value; _useOfPrice++;}
+        } 
+        #endregion
+        public static void PrintUsageStatistics()
+        {
+            System.Console.WriteLine($"There is {_numberOfCars} cars. Licenceplate is used {_useOfLicence} number of times. Price is used {_useOfPrice} number of times");
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StaticExamples
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            #region ListMethods test
+            List<int> aList = new List<int> { 23, 86, 51, 11, 39 };
+            // ListMethods listMethodsObject = new ListMethods();
+            Car Ferrari = new Car("504SKF", 299384);
+            Car BMW = new Car("738KDG", 5094593);
+            Car Porsche = new Car("326JSR", 98033966);
+            Car Audi = new Car("953FHT", 9695483);
+            
+            Car.PrintUsageStatistics();
+            System.Console.WriteLine($"{Ferrari.Price} {BMW.LicensePlate} {Porsche.Price} {Audi.Price}");
+            Car.PrintUsageStatistics();
+
+            System.Console.WriteLine($"The licenceplate is {Ferrari.LicensePlate} and the price is {Ferrari.Price}");
+
+            int smallest = ListMethods.FindSmallestNumber(aList);
+            Console.WriteLine($"The smallest number in the list is : {smallest}");
+
+            int average = ListMethods.FindAverage(aList);
+            Console.WriteLine($"The average of the list is : {average}"); 
+            #endregion
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
